5c4b9016467211b79870dfd1bed38e4583975211
Colman <colman.jin94@gmail.com>
2018-05-21 21:38:03 -0400

    Scrape functionality with scrape-it npm package

--- index.js
+++ index.js
@@ -3,6 +3,20 @@ const parser = require('./src/parse.js');
 
 
 function run(){
-    scraper('www.google.com');
+    console.log(process.argv[2]);
+    scraper(process.argv[2]);
+    
+    // scraper('www.google.com').then( (data)=>{
+    //     return new Promise(function(resolve, reject) {
+    //         fs.writeFile('data.json', JSON.parse(data), 'utf8', function(err){
+    //             if(err) reject(err);
+    //             else resolve(data);
+    //         });
+    //     });
+    // }).then(function(data){
+    //     console.log("results are ", data);
+    // }).catch(function(err){
+    //     console.log("Error: ", err);
+    // });
 }
 run();
--- src/scrape.js
+++ src/scrape.js
@@ -1,18 +1,114 @@
-const puppeteer = require('puppeteer');
+// const puppeteer = require('puppeteer');
+const scrapeIt = require("scrape-it");
+const fs = require('fs');
+// const urlString = 'www.google.com';
 
-const urlString = 'www.google.com';
+async function scrape(url){    
+    scrapeIt(url,{
+        awayStats: {
+            listItem: "div.col.gamepackage-away-wrap",
+            data: {
+                title: "div.team-name",
+                players: {
+                    listItem: "tr",
+                    data: {
+                        name: {
+                            selector: "td.name > a >span",
+                            how: "html"
+                        },
+                        stats:{
+                            listItem: "td",
+                            data:{
+                                key: {
+                                    attr: "class"
+                                },
+                                value: {
+                                    how: "html"
+                                }
+                                
+                            }
+                        }
+                    }
+                }
+            }
+        },
+        homeStats: {
+            listItem: "div.col.gamepackage-home-wrap",
+            data: {
+                title: "div.team-name",
+                players: {
+                    listItem: "tr",
+                    data: {
+                        name: {
+                            selector: "td.name > a >span",
+                            how: "html"
+                        },
+                        stats:{
+                            listItem: "td",
+                            data:{
+                                key: {
+                                    attr: "class"
+                                },
+                                value: {
+                                    how: "html"
+                                }
+                                
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }, (err, { data }) => {
+        console.log(err || data)
+        fs.writeFile('data.json', JSON.stringify(data), 'utf8', console.log('data,', data));
+        return data;
+    })
 
-async function scrape(url){
-    const browser = await puppeteer.launch({dumpio: true, args: ['--no-sandbox']});
-    
-    const page = await browser.newPage();
-    await page.goto(url);
-    console.log('going to ', url);
-    var pageContent = await page.content();
-    console.log(pageContent);
-    await browser.close();
+    // Callback interface
+    //scrapeIt("https://ionicabizau.net/", {
+        // Fetch the articles
+        // articles: {
+        //     listItem: ".article", 
+        //     data: {
+        //         // Get the article date and convert it into a Date object
+        //         createdAt: {
+        //             selector: ".date", 
+        //             convert: x => new Date(x)
+        //         },
+        //         // Get the title
+        //         title: "a.article-title",
+        //         // Nested list
+        //         tags: {
+        //             listItem: ".tags > span"
+        //         },
+        //         // Get the content
+        //         content: {
+        //             selector: ".article-content",
+        //             how: "html"
+        //         },
+        //         // Get attribute value of root listItem by omitting the selector
+        //         classes: {
+        //             attr: "class"
+        //         }
+        //     }
+        // },
+        // Fetch the blog pages
+        // pages: {
+        //     listItem: "li.page",
+        //     name: "pages",
+        //     data: {
+        //         title: "a",
+        //         url: {
+        //             selector: "a",
+        //             attr: "href"
+        //         }
+        //     }
+        // }
+    // }, (err, { data }) => {
+    //     console.log(err || data)
+    // })
 
-    return { url, pageContent};
 }
 
 module.exports = scrape;
